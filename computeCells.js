7a8c9d8777ca6cdf5f5c1b77acacaabb1dceafbd
José Luis Pereira <pepelui@outlook.com>
2019-05-09 18:30:57 -0300

    Added vue scaffold with webpack, eslint, mocha and chai

--- computeCells.js
+++ computeCells.js
@@ -1,24 +1,23 @@
-function computeCells(sled, hlines, vlines) {
-  let width = sled.image.width;
-  let height = sled.image.height;
+function computeCells (sled, hlines, vlines) {
+  let width = sled.image.width
+  let height = sled.image.height
   // order hlines and vlines
-  if((hlines.length > 1) && vlines.length > 1) {
-    let hLines = (hlines, 0);
-    sled.hlines = sled.hlines.sort(function(a, b){return a - b});
-    sled.vlines = sled.vlines.sort(function(a, b){return a - b});
+  if ((hlines.length > 1) && vlines.length > 1) {
+    let hLines = (hlines, 0)
+    sled.hlines = sled.hlines.sort(function (a, b) { return a - b })
+    sled.vlines = sled.vlines.sort(function (a, b) { return a - b })
     // compute intersections
-    let i = 0;    // horizontal (column) index
-    let j = 0;    // vertical (row) index
-    let ul, lr;   //upper left, lower right corners of cell
-    let cellIndex = 0, hRows = sled.hlines.length - 1, vCols = sled.vlines.length - 1; // one less row/column than lines
-    for(j = 0; j < hRows; j++) {
-      for(i = 0; i < vCols; i++) {
-        cellIndex = hRows * i + j;
-        ul = [sled.vlines[i], sled.hlines[j]];
-        lr = [sled.vlines[i + 1], sled.hlines[j + 1]];
-        sled.cells[cellIndex] = [ul, lr];
+    let i = 0 // horizontal (column) index
+    let j = 0 // vertical (row) index
+    let ul, lr // upper left, lower right corners of cell
+    let cellIndex = 0; let hRows = sled.hlines.length - 1; let vCols = sled.vlines.length - 1 // one less row/column than lines
+    for (j = 0; j < hRows; j++) {
+      for (i = 0; i < vCols; i++) {
+        cellIndex = hRows * i + j
+        ul = [sled.vlines[i], sled.hlines[j]]
+        lr = [sled.vlines[i + 1], sled.hlines[j + 1]]
+        sled.cells[cellIndex] = [ul, lr]
       }
     }
   }
 };
-
--- json_format.js
+++ json_format.js
@@ -1,15 +1,15 @@
-var sled;
+var sled
 sled = { image:
   {
-    width:  0,                  // horizontal extent of image in pixels
-    height: 0,                  // vertical extent of image
-    hlines: [0],                // y pixel coord of line
-    vlines: [0],                // x pixel coord of line
-    rows: 0,                    // count/length of rows - derived
-    columns: 0,                 // count/length of columns - derived
-    cells: [[[0, 0], [0, 0]]]   // pixel coord of upper left, lower right - derived
-  }                             // cell[row, column] = cell[row*columns + column]
-};
+    width: 0, // horizontal extent of image in pixels
+    height: 0, // vertical extent of image
+    hlines: [0], // y pixel coord of line
+    vlines: [0], // x pixel coord of line
+    rows: 0, // count/length of rows - derived
+    columns: 0, // count/length of columns - derived
+    cells: [[[0, 0], [0, 0]]] // pixel coord of upper left, lower right - derived
+  } // cell[row, column] = cell[row*columns + column]
+}
 /*
   intersections of hline and vlines determines rows, columns, and cells
   lines' centers will overlay pixel columns or rows, and will duplicate those pixels on adjoining cell edges
