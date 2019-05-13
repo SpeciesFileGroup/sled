1076b847ab5c17fead84c11e303460afd06e655d
jrflood <jrflood@illinois.edu>
2019-05-13 12:22:24 -0500

    #13 Vue integration first steps

--- app/javascript/vue/sled/app.vue
+++ app/javascript/vue/sled/app.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <div id="sled_test">
     <hi>SLED test page</hi>
     <div class="line-inputs">
-      horizontal line
+      new horizontal line<br>new vertical line
     </div>
   </div>
 </template>
