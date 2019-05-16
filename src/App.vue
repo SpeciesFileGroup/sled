12cd7149698c8e090e2196785b939883c3bd0b4f
José Luis Pereira <pepelui@outlook.com>
2019-05-16 17:53:30 -0300

    Fix

--- src/App.vue
+++ src/App.vue
@@ -30,10 +30,23 @@
     </div>
     <div class="cells">
       <table>
-        <tr v-for="(hline, hindex) in hlines">
+        <thead>
+          <tr>
+            <th></th>
+            <th v-for="(index) in vlines"> {{ index }} </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(hline, hindex) in hlines">
             <td v-html="hline"></td>
-            <td v-for="(vline, vindex) in vlines" v-html="cells[hlines.length*hindex + vindex]"></td>
-        </tr>
+            <template v-for="(vline, vindex) in vlines">
+              <td v-if="cells[(vlines.length*hindex) + vindex]">
+                {{ cells[(vlines.length*hindex) + vindex]}}
+              </td>
+              <td v-else/>
+            </template>
+          </tr>
+        </tbody>
       </table>
     </div>
   </div>
@@ -54,13 +67,15 @@ export default {
       width: 1000, // horizontal extent of image in pixels
       height: 750, // vertical extent of image
       hlines: [100, 200, 300], // y pixel coord of line
-      vlines: [100, 200], // x pixel coord of line
+      vlines: [50, 250], // x pixel coord of line
       cells: [] // pixel coord of upper left, lower right - derived, e.g.[[0, 0], [100, 150]]
     }
   },
+  mounted () {
+    this.computeCells()
+  },
   methods: {
     addNewHline (y) {
-      console.log('entraH')
       this.hlines.push(y)
       this.computeCells()
     },
@@ -68,7 +83,6 @@ export default {
       this.hlines = []
     },
     addNewVline (x) {
-      console.log('entraV')
       this.vlines.push(x)
       this.computeCells()
     },
@@ -76,24 +90,25 @@ export default {
       this.vlines = []
     },
     computeCells () {
-      if ((this.hlines.length > 1) && this.vlines.length > 1) {
-        this.hlines = this.hlines.sort(function (a, b) { return a - b})
-        this.vlines = this.vlines.sort(function (a, b) { return a - b})
+      if ((this.hlines.length > 0) && this.vlines.length > 0) {
+        this.hlines = this.hlines.sort(function (a, b) { return a - b })
+        this.vlines = this.vlines.sort(function (a, b) { return a - b })
         // compute intersections
         this.cells = []
-        let i = 0    // horizontal (column) index
-        let j = 0    // vertical (row) index
-        let ul, lr   //upper left, lower right corners of cell
+        let i = 0 // horizontal (column) index
+        let j = 0 // vertical (row) index
+        let ul, lr // upper left, lower right corners of cell
         let cellIndex = 0
-        let hRows = this.hlines.length - 1
-        let vCols = this.vlines.length - 1 // one less row/column than lines
+        let hRows = this.hlines.length
+        let vCols = this.vlines.length // one less row/column than lines
         for (j = 0; j < hRows; j++) {
           for (i = 0; i < vCols; i++) {
-            cellIndex = hRows * i + j
+            cellIndex = (vCols * j) + i
             ul = [this.vlines[i], this.hlines[j]]
             lr = [this.vlines[i + 1], this.hlines[j + 1]]
-            // this.cells[cellIndex] = [ul, lr];
-            this.cells.push([ul, lr])
+            if (lr[0] !== undefined && lr[1] !== undefined) {
+              this.cells[cellIndex] = [ul, lr]
+            }
           }
         }
       }
@@ -101,3 +116,10 @@ export default {
   }
 }
 </script>
+<style>
+  td {
+    width: 220px;
+    font-size: 12px;
+    border: 1px solid gray;
+  }
+</style>
